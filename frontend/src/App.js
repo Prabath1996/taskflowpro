0bea00ace858ba19515922a607611a2dd3f99e10
Prabath1996 <prabathkaushalya@outlook.com>
2025-04-27 10:45:43 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import LoginPage from './components/Login/LoginPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./pages/Login/LoginPage";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
+
 function App() {
   return (
-    <div className="App">
-     <LoginPage/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={LoginPage}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
