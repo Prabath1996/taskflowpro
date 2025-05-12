8bdff4f80ee351609323204bdd03cf8d7bf351c3
Prabath1996 <prabathkaushalya@outlook.com>
2025-05-12 22:19:36 +0530

    sidebar added

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
+import React from "react";
+import Header from "../../components/Header/Header";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
-const  Dashboard = () => {
+const Dashboard = () => {
   return (
     <>
-    <Header/>
-    <h1>Dashboard Page</h1>
-
-
+      <Header />
+      {/* <h1>Dashboard Page</h1> */}
+      <div className="main d-flex">
+        <div className="sidebarWrapper">
+          <Sidebar />
+        </div>
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default Dashboard
-
-
-
+export default Dashboard;
